dcc1c8ce7bf028f2d69790d5e994e19649115847
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 16:03:19 +0330

    Create and complete BaseServiceImpl.java class

---
+++ src/main/java/src/base/service/BaseServiceImpl.java
@@ -0,0 +1,69 @@
+package src.base.service;
+
+import lombok.AllArgsConstructor;
+import src.base.model.BaseEntity;
+import src.base.repository.BaseRepository;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class BaseServiceImpl <T extends BaseEntity<ID>, ID extends Serializable, R extends BaseRepository<T,ID>>
+        implements BaseService<T, ID> {
+    private final R repository;
+
+    @Override
+    public void save(T entity) {
+        repository.beginTransaction();
+        repository.save(entity);
+        repository.commitTransaction();
+    }
+
+    @Override
+    public void update(T entity) {
+        repository.beginTransaction();
+        repository.update(entity);
+        repository.commitTransaction();
+    }
+
+    @Override
+    public void delete(T entity) {
+        repository.beginTransaction();
+        repository.delete(entity);
+        repository.commitTransaction();
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Collection<T> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Collection<T> saveAll(Collection<T> entityCollection) {
+        repository.beginTransaction();
+        Collection<T> saveList = repository.saveAll(entityCollection);
+        repository.commitTransaction();
+        return saveList;
+    }
+
+    @Override
+    public void beginTransaction() {
+        repository.beginTransaction();
+    }
+
+    @Override
+    public void commitTransaction() {
+        repository.commitTransaction();
+    }
+
+    @Override
+    public void rollBack() {
+        repository.rollBack();
+    }
+}
