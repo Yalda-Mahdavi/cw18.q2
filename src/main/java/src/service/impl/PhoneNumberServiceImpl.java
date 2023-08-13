47f5458942c2acf15bb63da4091135774d7a3473
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 10:52:11 +0330

    Add constructor to PhoneNumberServiceImpl.java class

--- src/main/java/src/service/impl/PhoneNumberServiceImpl.java
+++ src/main/java/src/service/impl/PhoneNumberServiceImpl.java
@@ -1,4 +1,11 @@
 package src.service.impl;
 
+import src.repository.impl.PhoneNumberRepositoryImpl;
+
 public class PhoneNumberServiceImpl {
+    private PhoneNumberRepositoryImpl phoneNumberRepository;
+
+    public PhoneNumberServiceImpl(PhoneNumberRepositoryImpl phoneNumberRepository) {
+        this.phoneNumberRepository = phoneNumberRepository;
+    }
 }
