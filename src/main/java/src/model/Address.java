2228e70a4a29d13d92f30031d0a856bbc2e6c047
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 10:32:30 +0330

    Add Address.java class

--- src/main/java/src/model/Address.java
+++ src/main/java/src/model/Address.java
@@ -1,16 +1,38 @@
 package src.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
+
 @Entity
+@Table(name = "address")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private long postalCode;
+    private String postalAddress;
+    private String city;
+
+    @ManyToOne
+    private Employee employee;
+
+    @OneToMany (mappedBy = "address", cascade = CascadeType.ALL)
+    private List<PhoneNumber> phoneNumbers;
+
+    public Address(long postalCode, String postalAddress, String city, Employee employee, List<PhoneNumber> phoneNumbers) {
+        this.postalCode = postalCode;
+        this.postalAddress = postalAddress;
+        this.city = city;
+        this.employee = employee;
+        this.phoneNumbers = phoneNumbers;
+    }
 }
