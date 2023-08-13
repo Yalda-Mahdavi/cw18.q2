e7d0c589c378b71471e8ea922ae940cc27ec60c8
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 10:40:20 +0330

    Add EmployeeRepository.java interface

--- src/main/java/src/repository/EmployeeRepository.java
+++ src/main/java/src/repository/EmployeeRepository.java
@@ -1,4 +1,16 @@
 package src.repository;
 
+import src.model.Employee;
+
+import java.util.List;
+
 public interface EmployeeRepository {
+    void save(Employee employee);
+    void update(Employee employee);
+    void remove(Employee employee);
+    Employee findById(Long id);
+    Employee findEmployeesByPostalCode(long postalCode);
+    Employee findEmployeesByTelNumber(String telNumber);
+    List<Employee> findEmployeesByCity(String city);
+    List<Employee> findEmployeesByMobNumber(String mobNumber);
 }
