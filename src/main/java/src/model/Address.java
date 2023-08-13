c394e7f8430be60d4706145853e84dac1d2b32b0
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 16:13:12 +0330

    Make Address.java class generic

--- src/main/java/src/model/Address.java
+++ src/main/java/src/model/Address.java
@@ -2,6 +2,7 @@ package src.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import src.base.model.BaseEntity;
 
 import java.util.List;
 
@@ -13,10 +14,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "address")
-public class Address {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class Address extends BaseEntity<Long> {
 
     private long postalCode;
     private String postalAddress;
@@ -28,7 +26,8 @@ public class Address {
     @OneToMany(mappedBy = "address", cascade = CascadeType.ALL)
     private List<PhoneNumber> phoneNumbers;
 
-    public Address(long postalCode, String postalAddress, String city, Employee employee, List<PhoneNumber> phoneNumbers) {
+    public Address(Long id, long postalCode, String postalAddress, String city, Employee employee, List<PhoneNumber> phoneNumbers) {
+        super(id);
         this.postalCode = postalCode;
         this.postalAddress = postalAddress;
         this.city = city;
