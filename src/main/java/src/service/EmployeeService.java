3e2b52ee0ac8d611239b0ad74813a0ced7d148e0
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 10:49:16 +0330

    Add methods to EmployeeService.java interface

--- src/main/java/src/service/EmployeeService.java
+++ src/main/java/src/service/EmployeeService.java
@@ -1,4 +1,16 @@
 package src.service;
 
+import src.model.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+    void saveEmployee(Employee employee);
+    void updateEmployee(Employee employee);
+    void removeEmployee(Employee employee);
+    Employee findEmployeeById(Long id);
+    Employee findEmployeesByPostalCode(long postalCode);
+    Employee findEmployeesByTelNumber(String telNumber);
+    public List<Employee> findEmployeesByCity(String city);
+    List<Employee> findEmployeesByMobNumber(String mobNumber);
 }
