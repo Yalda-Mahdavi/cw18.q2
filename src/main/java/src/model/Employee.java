a888c441963c76bdf32f10a8d788d9dc90e71e59
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 16:15:11 +0330

    Make Employee.java class generic

--- src/main/java/src/model/Employee.java
+++ src/main/java/src/model/Employee.java
@@ -2,6 +2,8 @@ package src.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import src.base.model.BaseEntity;
+
 import java.util.List;
 
 @Getter
@@ -12,11 +14,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "employee")
-public class Employee {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+public class Employee extends BaseEntity<Long> {
     private String name;
     private String empCode;
     private long salary;
@@ -24,7 +22,8 @@ public class Employee {
     @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
     private List<Address> addresses;
 
-    public Employee(String name, String empCode, long salary, List<Address> addresses) {
+    public Employee(Long id, String name, String empCode, long salary, List<Address> addresses) {
+        super(id);
         this.name = name;
         this.empCode = empCode;
         this.salary = salary;
@@ -34,8 +33,7 @@ public class Employee {
     @Override
     public String toString() {
         return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
+                "name='" + name + '\'' +
                 ", empCode='" + empCode + '\'' +
                 ", salary=" + salary +
                 '}';
