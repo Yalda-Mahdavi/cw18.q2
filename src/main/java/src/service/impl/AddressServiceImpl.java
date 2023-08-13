fd1e77c6247f024799ab1aa907859e6cbd7c59b8
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 10:46:19 +0330

    Add constructor to AddressServiceImpl.java class

--- src/main/java/src/service/impl/AddressServiceImpl.java
+++ src/main/java/src/service/impl/AddressServiceImpl.java
@@ -1,4 +1,11 @@
 package src.service.impl;
 
+import src.repository.impl.AddressRepositoryImpl;
+
 public class AddressServiceImpl {
+    private AddressRepositoryImpl addressRepository;
+
+    public AddressServiceImpl(AddressRepositoryImpl addressRepository) {
+        this.addressRepository = addressRepository;
+    }
 }
