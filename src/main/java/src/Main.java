82ca2a892356aec0c11c6d519c580fb1b9a48a07
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 11:02:47 +0330

    Add cw18.q2 files

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -1,7 +1,43 @@
 package src;
 
-public class Main {
-    public static void main(String[] args) {
+import src.model.Address;
+import src.model.Employee;
+import src.model.PhoneNumber;
+import src.service.EmployeeService;
+import src.util.ApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main( String[] args )
+    {
+        EmployeeService empService = ApplicationContext.getEmpService();
+        ApplicationContext.getAddressService();
+        ApplicationContext.getPhoneNumService();
+
+        Employee employee = new Employee();
+        employee.setName("Ahmad");
+        employee.setSalary(100L);
+        employee.setEmpCode("0031");
+
+        List<PhoneNumber> phoneNums = new ArrayList<>();
+
+        Address address1 = new Address(198L, "P address1", "Tehran", employee, phoneNums);
+        PhoneNumber phoneNum1 = new PhoneNumber("021", "0910", address1);
+
+        phoneNums.add(phoneNum1);
+
+        List<Address> addresses = new ArrayList<>();
+        addresses.add(address1);
+
+        employee.setAddresses(addresses);
+
+        empService.saveEmployee(employee);
+//        empService.removeEmployee(empService.findEmployeeById(2L));
+
+        System.out.println(empService.findEmployeesByPostalCode(198L));
+        System.out.println(empService.findEmployeesByTelNumber("021"));
+        System.out.println("Employees in Tehran: " + empService.findEmployeesByCity("Tehran"));
     }
 }
