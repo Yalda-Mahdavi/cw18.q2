6a8f5e4d42eff802c446924564df71db948d7c82
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 13:20:15 +0330

    Create and complete BaseRepository.java interface

---
+++ src/main/java/src/base/repository/BaseRepository.java
@@ -0,0 +1,16 @@
+package src.base.repository;
+
+import src.base.model.BaseEntity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Optional;
+
+public interface BaseRepository<T extends BaseEntity<ID>, ID extends Serializable> {
+    void save (T entity);
+    void update (T entity);
+    void delete (T entity);
+    Optional<T> findById(ID id);
+Collection<T> findAll();
+Collection<T> saveAll (Collection<T> entityCollection);
+}
