3434f608d2a4822eb8cfe0aa5b21e34c0fc06d81
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 16:02:36 +0330

    Create and complete BaseService.java interface

---
+++ src/main/java/src/base/service/BaseService.java
@@ -0,0 +1,17 @@
+package src.base.service;
+import src.base.model.BaseEntity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Optional;
+public interface BaseService<T extends BaseEntity<ID>, ID extends Serializable> {
+    void save (T entity);
+    void update (T entity);
+    void delete (T entity);
+    Optional<T> findById(ID id);
+    Collection<T> findAll();
+    Collection<T> saveAll (Collection<T> entityCollection);
+    void beginTransaction();
+    void commitTransaction();
+    void rollBack();
+}
