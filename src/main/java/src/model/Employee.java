2e79f23fdeff11fe6f20db4f4f532d1127380633
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 10:33:42 +0330

    Add Employee.java class

--- src/main/java/src/model/Employee.java
+++ src/main/java/src/model/Employee.java
@@ -1,16 +1,44 @@
 package src.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+
+
 @Entity
+@Table(name = "employee")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String empCode;
+    private long salary;
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+    private List<Address> addresses;
+
+    public Employee(String name, String empCode, long salary, List<Address> addresses) {
+        this.name = name;
+        this.empCode = empCode;
+        this.salary = salary;
+        this.addresses = addresses;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", empCode='" + empCode + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
