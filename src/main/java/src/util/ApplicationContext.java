6a7e357f7e69477d6f26ba209973d5518fed58e0
Yalda Mahdavi <yaldamahdavi12.yam@gmail.com>
2023-08-13 10:59:40 +0330

    Add and complete ApplicationContext.java class

---
+++ src/main/java/src/util/ApplicationContext.java
@@ -0,0 +1,52 @@
+package src.util;
+
+import jakarta.persistence.EntityManager;
+import src.repository.impl.AddressRepositoryImpl;
+import src.repository.impl.EmployeeRepositoryImpl;
+import src.repository.impl.PhoneNumberRepositoryImpl;
+import src.service.impl.AddressServiceImpl;
+import src.service.impl.EmployeeServiceImpl;
+import src.service.impl.PhoneNumberServiceImpl;
+
+public class ApplicationContext {
+
+
+    private static EntityManager em;
+
+    private static EmployeeRepositoryImpl employeeRepository;
+    private static EmployeeServiceImpl employeeService;
+
+    private static AddressRepositoryImpl addressRepository;
+    private static AddressServiceImpl addressService;
+
+    private static PhoneNumberRepositoryImpl phoneNumberRepository;
+    private static PhoneNumberServiceImpl phoneNumberService;
+
+
+
+    static {
+        em = HibernateUtils.getEntityManagerFactory().createEntityManager();
+
+        employeeRepository = new EmployeeRepositoryImpl(em);
+        employeeService = new EmployeeServiceImpl(employeeRepository);
+
+        addressRepository = new AddressRepositoryImpl(em);
+        addressService = new AddressServiceImpl(addressRepository);
+
+        phoneNumberRepository = new PhoneNumberRepositoryImpl(em);
+        phoneNumberService = new PhoneNumberServiceImpl(phoneNumberRepository);
+    }
+
+    public static EmployeeServiceImpl getEmpService() {
+        return employeeService;
+    }
+
+    public static AddressServiceImpl getAddressService() {
+        return addressService;
+    }
+
+    public static PhoneNumberServiceImpl getPhoneNumService() {
+        return phoneNumberService;
+    }
+
+}
